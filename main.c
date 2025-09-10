fef9579c04216011bca84e9ce7fc6d564db132e6
Paulo <=>
2025-09-10 20:16:58 -0300

    update main

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ void sum(){
     scanf("%f", &a);
     printf("Digite o segundo número: ");
     scanf("%f", &b);
-    printf("Resultado: %.2f\n", a + b);
+    printf("Resultado: %.2f + %.2f = %.2f\n", a, b, a + b);
 }
 
 void subtract(){
@@ -17,7 +17,7 @@ void subtract(){
     scanf("%f", &a);
     printf("Digite o segundo número: ");
     scanf("%f", &b);
-    printf("Resultado: %.2f\n", a - b);
+    printf("Resultado: %.2f - %.2f = %.2f\n", a, b, a - b);
 }
 
 void multiply(){
@@ -26,7 +26,7 @@ void multiply(){
     scanf("%f", &a);
     printf("Digite o segundo número: ");
     scanf("%f", &b);
-    printf("Resultado: %.2f\n", a * b);
+    printf("Resultado: %.2f x %.2f = %.2f\n", a, b, a * b);
 }
 
 void divide(){
@@ -39,11 +39,11 @@ void divide(){
         printf("Erro: Divisão por zero não é permitida.\n");
         return;
     }
-    printf("Resultado: %.2f\n", a / b);
+    printf("Resultado: %.2f / %.2f = %.2f\n", a, b, a / b);
 }
 
 int callMenu(){
-    int option;
+    char option;
     printf("===============================\n");
     printf("   Calculadora Simples\n");
     printf("===============================\n");
@@ -54,8 +54,12 @@ int callMenu(){
     printf("4. Divisão\n");
     printf("5. Sair\n");
     printf("Opção:");
-    scanf("%d", &option);
-    return option;
+    scanf("%c", &option);
+    while(option != '1' && option != '2' && option != '3' && option != '4' && option != '5'){
+        printf("A resposta é inválida, coloque um número de 1 a 5: ");
+        scanf(" %c", &option);
+    }
+    return option - '0';
 }
 
 int main(){
@@ -75,12 +79,18 @@ int main(){
             case 4:
                 divide();
                 break;
+            default:
+                printf("Obrigado por usar a calculadora! Até a próxima.");
         }
+        if(option == 5) break;
         printf("Deseja realizar outra operação? (s/n): ");
         char c; scanf(" %c", &c);
-        if(tolower(c) == 's' ){
-            continue;
-        } else {
+        while(tolower(c) != 's' && tolower(c) != 'n') {
+            printf("Entrada incorreta, por favor insira s ou n: ");
+            scanf(" %c", &c);
+        }
+
+        if(tolower(c) == 'n'){
             printf("Obrigado por usar a calculadora! Até a próxima.");
             option = 5;
         }
